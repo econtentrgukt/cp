2edf7cb56cf891df8e617f05d39d190e66043359
B V CHARAN KUMAR <120728597+bvcharan26@users.noreply.github.com>
2023-09-28 15:27:47 +0530

    Created website.html

---
+++ social.js
@@ -0,0 +1,9 @@
+function whatsapp(){
+    window.location.href='https://chat.whatsapp.com/DYTXCYVnv96GZiFJDMBA0W';
+}
+function github(){
+    window.location.href='https://github.com/bvcharan26';
+}
+function mail(){
+    window.location.href='mailto:charanbanala26@gmail.com';
+}
